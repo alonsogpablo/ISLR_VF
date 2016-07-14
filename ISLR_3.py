b2bb61967b6f1b712003147ff52e700847b484ad
Pablo <alonsogpablo@gmail.com>
2016-07-14 13:13:32 +0200

    Initial commit

---
+++ ISLR_3.py
@@ -0,0 +1,24 @@
+# %load ../standard_import.txt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+import seaborn as sns
+
+from sklearn.preprocessing import scale
+import sklearn.linear_model as skl_lm
+from sklearn.metrics import mean_squared_error, r2_score
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+pd.set_option('display.notebook_repr_html', False)
+
+plt.style.use('seaborn-white')
+
+advertising = pd.read_csv('/Users/Pablo/PycharmProjects/ISLR/Advertising.csv', usecols=[1,2,3,4])
+advertising.info()
+
+sns.regplot(advertising.TV, advertising.Sales, order=1, ci=None, scatter_kws={'color':'r'})
+plt.xlim(-10,310)
+plt.ylim(ymin=0);
+plt.show()
